47eda688c497157a888733be3457655afaee0742
Andrew Azores <me@andrewazor.es>
2025-07-14 17:10:13 -0400

    Revert "do not apply acrylic effect on windows 10"
    This reverts commit 68606fe9cf6c4b0dc4b6ffa75794133d325b4fbb.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1277,11 +1277,8 @@ fn update_window_effect(theme: String, window: tauri::Window) {
         } else {
             Some((26, 29, 27, 60))
         };
-
-        if os_info::get().version().to_string().starts_with("11.") {
-            window_vibrancy::apply_acrylic(&window, color)
-                .expect("Unsupported platform! 'apply_acrylic' is only supported on Windows");
-        }
+        window_vibrancy::apply_acrylic(&window, color)
+            .expect("Unsupported platform! 'apply_acrylic' is only supported on Windows");
     }
 
     #[cfg(target_os = "macos")]
@@ -1423,17 +1420,16 @@ fn main() {
 
                 #[cfg(target_os = "windows")]
                 {
-                    let color = match theme.as_str() {
-                        "light" => Some((250, 250, 250, 150)),
-                        "muted-green" => Some((44, 56, 54, 100)),
-                        _ => Some((26, 29, 27, 60))
+                    let color = if theme == "light" {
+                        Some((250, 250, 250, 150))
+                    } else if theme == "muted-green" {
+                        Some((44, 56, 54, 100))
+                    } else {
+                        Some((26, 29, 27, 60))
                     };
-                    let os = os_info::get();
-                    if os.version().to_string().starts_with("11.") {
-                        apply_acrylic(&window, color).expect(
-                            "Unsupported platform! 'apply_acrylic' is only supported on Windows",
-                        );
-                    }
+                    apply_acrylic(&window, color).expect(
+                        "Unsupported platform! 'apply_acrylic' is only supported on Windows",
+                    );
                 }
             }
 
